bc102b1de1459a4075a115e549a0c1efe8457e61
bhaskar reddy <45232937+Bhaskarrd10@users.noreply.github.com>
2022-05-11 09:30:34 +0000

    123

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+ "Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+print(programming_dictionary["Bug"])
+
+
+programming_dictionary["reddy"] = "halfsdlfkjlsdfjklsfjdfaljfdlkf"
+
+print(programming_dictionary)
+
+#programming_dictionary= {}
+#print(programming_dictionary)
